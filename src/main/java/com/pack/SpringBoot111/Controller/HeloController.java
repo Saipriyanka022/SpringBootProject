6c95bb369a6af62f36e23dff958184a4948bc938
SAI PRIYANKA.S <saipriyankasbme2019@citchennai.net>
2023-06-23 12:10:33 +0530

    first commit

---
+++ src/main/java/com/pack/SpringBoot111/Controller/HeloController.java
@@ -0,0 +1,15 @@
+package com.pack.SpringBoot111.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+
+public class HeloController {
+	@GetMapping("/hello")
+	public String  getMessage() {
+		return "HelloWorld";
+		
+	}
+}
---
+++ src/main/java/com/pack/SpringBoot111/SpringBoot111Application.java
@@ -0,0 +1,13 @@
+package com.pack.SpringBoot111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot111Application.class, args);
+	}
+
+}
